68a29d45fa9756e53556762eb90a5b1da5e69a76
Erunn <186409238+Erunn@users.noreply.github.com>
2026-03-11 23:42:53 +0000

    Update script.js

--- script.js
+++ script.js
@@ -169,6 +169,8 @@ const UI = {
             
             const emoji = d.emojiLibrary?.[d.emoji?.toLowerCase()];
             const emojiHTML = emoji ? ` <span style="font-style: normal;">${emoji}</span>` : "";
+            
+            // THE FIX IS RIGHT HERE:
             if (this.dom['event-name']) this.dom['event-name'].innerHTML = `${d.eventName}${emojiHTML}`;
             
             if (Number(d.useTimer) === 1 && d.targetDate) this.runTimer(d.targetDate, d.celebrationMessage);
